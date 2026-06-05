1b6c9e7c4decc937b24a3caf86e5938a897f5ee3
posicionaando-maker <posiciona.ando@gmail.com>
2026-06-05 17:02:23 -0400

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'breakloop-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/app.js',
+    '/manifest.json'
+];
+
+// Instalación: cachear archivos
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Fetch: responder con caché o red
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
+
+// Activación: limpiar caches viejos
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
